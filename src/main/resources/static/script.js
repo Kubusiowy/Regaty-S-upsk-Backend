1117c926d7b04df3b46cb0349daf8a8cf9bf9ffb
Kuba <firkowskikuba@gmail.com>
2026-06-08 01:46:25 +0200

    time in chart is displayed in ms

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,23 +1,56 @@
-const ctx = document.getElementById("resultsChart");
+function formatTime(ms) {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+
+
+    const centiseconds = Math.round(ms % 1000);
+
+    return `${minutes}:${seconds.toString().padStart(2, "0")}.${centiseconds.toString().padStart(2, "0")}`;
+}
+
+const ctx = document.getElementById("resultsChart")
 
 new Chart(ctx, {
     type: "bar",
     data: {
-        labels: ["LO1", "LO2", "LO3", "LO4", "LO5", "Rolniczak", "ELEKTRYK"],
+        labels: ["LO1", "LO2", "LO3", "LO4", "LO5"],
         datasets: [{
-            label: "Sekundy",
-            data: [82, 74, 23, 54, 21, 67, 48],
-            backgroundColor: ["#0f7c90", "#ef8f38", "#14213d", "#0f7c90", "#ef8f38", "#14213d", "#0a4f66"],
+            label: "Czas",
+            data: [75321, 74210, 81234, 65432, 70211],
+            backgroundColor: [
+                "#0f7c90",
+                "#ef8f38",
+                "#14213d",
+                "#0f7c90",
+                "#ef8f38"
+            ],
             borderRadius: 6
         }]
     },
     options: {
         indexAxis: "y",
         responsive: true,
+
         scales: {
             x: {
-                beginAtZero: true
+                beginAtZero: true,
+
+                ticks: {
+                    callback: function(value) {
+                        return formatTime(value)
+                    }
+                }
+            }
+        },
+
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return formatTime(context.raw)
+                    }
+                }
             }
         }
     }
-});
+})
