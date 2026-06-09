9471fa3fe8f8b3dd43ea8860559e84577ba615a8
firkowski <firkowskikuba@gmail.com>
2026-06-09 10:23:49 +0200

    finished repo in scores feature

--- src/main/kotlin/features/scores/repository/ResultRepository.kt
+++ src/main/kotlin/features/scores/repository/ResultRepository.kt
@@ -1,5 +1,6 @@
 package com.example.features.scores.repository
 
+import com.example.features.scores.dto.request.UpdateResultRequest
 import com.example.features.scores.dto.response.ResultResponse
 import com.example.features.scores.model.ResultModel
 import java.util.UUID
@@ -7,12 +8,12 @@ import java.util.UUID
 interface ResultRepository {
 
     suspend fun createResult(resultModel: ResultModel): ResultModel
+
     suspend fun updateResult(
-        resultId: UUID,
-        timeMs: Long,
+        resultModel: ResultModel,
     ): ResultModel
 
     suspend fun getAllResults(): List<ResultModel>
-
-    suspend fun delete(id: UUID)
+    suspend fun getResult(id: UUID): ResultModel?
+    suspend fun delete(id: UUID):Int
 }
--- src/main/kotlin/features/scores/repository/ResultRepositoryImpl.kt
+++ src/main/kotlin/features/scores/repository/ResultRepositoryImpl.kt
@@ -2,11 +2,17 @@ package com.example.features.scores.repository
 
 import com.example.core.database.dbQuery
 import com.example.core.database.tables.Results
+import com.example.core.plugins.exception.BadRequestException
+import com.example.features.scores.dto.request.UpdateResultRequest
 import com.example.features.scores.dto.response.ResultResponse
 import com.example.features.scores.model.ResultModel
 import com.example.features.scores.repository.mapping.toResultModel
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.javatime.time
 import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.update
 import java.util.UUID
 
 class ResultRepositoryImpl : ResultRepository {
@@ -24,26 +30,35 @@ class ResultRepositoryImpl : ResultRepository {
     }
 
     override suspend fun updateResult(
-        resultId: UUID,
-        timeMs: Long
-    ): ResultModel {
-        dbQuery {
-            TODO()
+        resultModel: ResultModel,
+    ): ResultModel = dbQuery {
+        val updatedRows = Results.update(
+            where = { Results.id eq resultModel.id.toString()}
+        ) {
+            it[timeMs] = resultModel.timeMs
         }
+        if(updatedRows == 0) throw BadRequestException("school with id ${resultModel.id} was not found")
+        resultModel
     }
 
-    override suspend fun getAllResults(): List<ResultModel> {
-       return dbQuery {
+
+
+    override suspend fun getAllResults(): List<ResultModel> = dbQuery {
             Results
                 .selectAll()
-                .map {
-                    it.toResultModel()
-                }
+                .map {it.toResultModel()}
         }
+
+
+    override suspend fun getResult(id: UUID): ResultModel? =dbQuery {
+        Results
+            .selectAll()
+            .where{ Results.id eq id.toString() }
+            .singleOrNull()?.toResultModel()
     }
 
-    override suspend fun delete(id: UUID) {
-        TODO("Not yet implemented")
+    override suspend fun delete(id: UUID): Int = dbQuery {
+        Results.deleteWhere {Results.id eq id.toString()}
     }
 
 }
